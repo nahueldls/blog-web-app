2e9719542f64e5ba10cbc4cf6967ce0424317d89
Nahuel De Los Santos <nahueldelossantos646@gmail.com>
2024-11-27 07:27:46 -0300

    Add bootsrap via cdn and a navbar with links. Still there is errors to fix.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import {fileURLToPath} from "url";
+import path from 'path';
+import { dirname } from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 let arrpost = [];
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', (req,res) => {
     res.render("index.ejs");
+});
+
+
+app.get('/post', (req, res) => {
+    res.render("partials/post.ejs");
 });
 
 app.post('/submit', (req, res) => {
